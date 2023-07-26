50f1a3d52a3c81997aa7f5fe934e023cab3266d1
wh-freestyle <454372070@qq.com>
2023-07-26 14:50:12 +0800

    update

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "测试一下下";
+header("Content-type: application/json");
+$return = [
+    [
+        'id' => 1,
+        'image' => 'https://assets.zhayieye.com/news/data/article/2023_07_25/0/11690273559_nl.jpg',
+        'link' => ''
+    ],
+    [
+        'id' => 2,
+        'image' => 'https://assets.zhayieye.com/news/data/article/2023_07_26/6/pic_1690353656_11931.jpg',
+        'link' => 'https://www.baidu.com'
+    ]
+];
+$string = json_encode($return, JSON_UNESCAPED_SLASHES);
+echo $string;
