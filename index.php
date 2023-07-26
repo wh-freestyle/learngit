0644e304f72ef6b36aa9d2e20b1a88ee385c668d
wh-freestyle <454372070@qq.com>
2023-07-26 15:00:56 +0800

    update

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-header("Content-type: application/json");
-$return = [
-    [
-        'id' => 1,
-        'image' => 'https://assets.zhayieye.com/news/data/article/2023_07_25/0/11690273559_nl.jpg',
-        'link' => ''
-    ],
-    [
-        'id' => 2,
-        'image' => 'https://assets.zhayieye.com/news/data/article/2023_07_26/6/pic_1690353656_11931.jpg',
-        'link' => 'https://www.baidu.com'
-    ]
-];
-$string = json_encode($return, JSON_UNESCAPED_SLASHES);
-echo $string;
+echo '测试一下下';
