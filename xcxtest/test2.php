f301ce0c1cdcfafa02ec23e6baf5cb715fcb58c8
wh-freestyle <454372070@qq.com>
2023-08-01 11:23:23 +0800

    update

--- xcxtest/test2.php
+++ xcxtest/test2.php
@@ -14,37 +14,37 @@ $return = [
     [
         'id' => 3,
         'name' => 'icon3',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 4,
         'name' => 'icon4',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 5,
         'name' => 'icon5',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 6,
         'name' => 'icon6',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 7,
         'name' => 'icon7',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 8,
         'name' => 'icon8',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ],
     [
         'id' => 9,
         'name' => 'icon9',
-        'link' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
+        'icon' => 'https://assets.zhayieye.com/app/mp_home_icon_job2x.png'
     ]
 ];
 $string = json_encode($return, JSON_UNESCAPED_SLASHES);
